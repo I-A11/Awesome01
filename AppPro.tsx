29e8bdbb08b8d438bc29bf55aefc1ba4e1542ce4
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-12 15:55:20 +1000

    Start create React-native app using React Native cli.

---
+++ AppPro.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+
+const AppPro = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default AppPro;
