f3162f9681f9b5884a8d7699eecbae5d7628b453
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-13 14:31:41 +1000

    Add color scheme to change the text color depending on the mode.

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  SafeAreaView,
+} from 'react-native';
 
-const AppPro = () => {
+const AppPro = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+          Hello World !
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: '#ffffff',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
 
 export default AppPro;
